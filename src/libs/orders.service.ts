50d01ba8f4cde35680d7650c81d6a1ce54d60854
Nick Felix <nicolas.felixa@gmail.com>
2021-11-04 18:06:56 -0300

    Small refactor

--- src/libs/orders.service.ts
+++ src/libs/orders.service.ts
@@ -2,14 +2,14 @@ import Websocket from "../modules/websocket/websocket";
 
 class OrdersService {
 
-    getOrders() {
-
-    }
-
     public insertOrder(order) {
         //save in your database
 
         //send the update to the browser
+        this.updateSockets(order);
+    }
+
+    private updateSockets(order) {
         const io = Websocket.getInstance();
         io.of('orders').emit('orders_updated', { data: [order] });
     }
